9f81d71940597986e1fef7fc97734ae99eade240
nikawa2161 <nikawa.2161.t@gmail.com>
2024-09-22 06:19:01 +0900

    fix:コピーライト

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,15 +48,7 @@ export default function RootLayout({
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
+                  © 2024 Team Meguro Ecstasy. All rights reserved.
                 </p>
                 <ThemeSwitcher />
               </footer>
