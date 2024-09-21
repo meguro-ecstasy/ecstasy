3b66e9a377cbff29b6a51c1550d83b5794029b86
Naoki Takahashi <naoki.tkb@gmail.com>
2024-09-22 02:33:07 +0900

    feat: add form action

---
+++ app/protected/post-question/_actions/post-message.action.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { createClient } from '@/utils/supabase/server';
+import { parseWithZod } from '@conform-to/zod';
+import { postMessageSchema } from '../_models';
+import { redirect } from 'next/navigation';
+
+export const postMessageAction = async (
+  prevState: unknown,
+  formData: FormData,
+) => {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return redirect('/sign-in');
+  }
+
+  const submission = parseWithZod(formData, {
+    schema: postMessageSchema,
+  });
+
+  if (submission.status !== 'success') {
+    return submission.reply();
+  }
+
+  await supabase.from('questions').insert({
+    userId: user.id,
+    tagId: submission.value.tagId,
+    content: submission.value.content,
+  });
+
+  // フォームをリセットする
+  return submission.reply({ resetForm: true });
+};
