580fab45fcf96c49a6c87734299dca9fd0444ad1
Naoki Takahashi <naoki.tkb@gmail.com>
2024-09-22 11:39:25 +0900

    メッセージをリアルタイムで更新されるように修正！

--- app/protected/message/_containers/index.ts
+++ app/protected/message/_containers/index.ts
@@ -1,1 +1,2 @@
 export * from './message.container';
+export * from './post.message';
--- app/protected/message/_containers/message.container.tsx
+++ app/protected/message/_containers/message.container.tsx
@@ -2,11 +2,13 @@
 
 import { useMessageFacade } from './message.facade';
 import { MessageListComponent } from '../_components';
+import { ComponentProps } from 'react';
 
-type Props = Parameters<typeof useMessageFacade>[0];
+type Props = Parameters<typeof useMessageFacade>[0] &
+  Pick<ComponentProps<typeof MessageListComponent>, 'userId'>;
 
-export const MessageContainer = ({ questionId }: Props) => {
-  const { messages, userId } = useMessageFacade({ questionId });
+export const MessageContainer = ({ questionId, userId }: Props) => {
+  const { messages } = useMessageFacade({ questionId });
 
   return <MessageListComponent messages={messages} userId={userId} />;
 };
