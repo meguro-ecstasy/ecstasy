e803efaf8d07b6ae28cce8f5536145a0f36b6352
Kenya Masuko <ken12rug@gmail.com>
2024-09-22 09:40:45 +0900

    tag

--- app/protected/user/setting/page.tsx
+++ app/protected/user/setting/page.tsx
@@ -6,7 +6,7 @@ import { notFound } from 'next/navigation';
 
 export default async function Page() {
   const supabase = createClient();
-  const { data: d, error: e } = await await supabase.from('tags').select('*');
+  const { data: d } = await await supabase.from('tags').select('*');
   const options = d?.map((t) => ({ label: t.name, value: t.id.toString() }));
   const user = await supabase.auth.getUser();
 
@@ -14,21 +14,18 @@ export default async function Page() {
     return notFound();
   }
 
-  const { data, error: _ } = await supabase
+  const { data } = await supabase
     .from('users')
-    .select('tags!user_tags(*)')
+    .select('tags(*)')
     .eq('id', user.data.user?.id)
     .single();
-  const tag = data;
+  const tag = data?.tags;
 
   if (!options) {
     return notFound();
   }
 
   return (
-    <Form
-      options={options}
-      defaultValue={tag?.tags ? tag.tags[0].id.toString() : null}
-    />
+    <Form options={options} defaultValue={tag ? tag.id.toString() : null} />
   );
 }
