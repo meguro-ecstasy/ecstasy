e554100ce0d5461e1a1dc0d4932bbf42cd4c8835
Naoki Takahashi <naoki.tkb@gmail.com>
2024-09-22 12:46:03 +0900

    fix

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -4,19 +4,18 @@ import { Button } from '@/components/ui/button';
 import { type ComponentProps } from 'react';
 import { useFormStatus } from 'react-dom';
 
-type Props = ComponentProps<typeof Button> & {
-  pendingText?: string;
-};
+type Props = ComponentProps<typeof Button>;
 
-export function SubmitButton({
-  children,
-  pendingText = 'Submitting...',
-  ...props
-}: Props) {
+export function SubmitButton({ children, ...props }: Props) {
   const { pending } = useFormStatus();
 
   return (
-    <Button className="w-full text-lg" disabled={pending} type="submit">
+    <Button
+      className="w-full text-lg"
+      disabled={pending}
+      type="submit"
+      {...props}
+    >
       {pending ? (
         <div className="flex justify-center" aria-label="読み込み中">
           <div className="animate-spin h-6 w-6 border-4 border-white rounded-full border-t-transparent"></div>
