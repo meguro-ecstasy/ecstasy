bceae523eec37d644817bf8665dcc948b87ae2bf
Naoki Takahashi <naoki.tkb@gmail.com>
2024-09-22 11:42:08 +0900

    fix typo

--- app/protected/message/_containers/post.message.tsx
+++ app/protected/message/_containers/post.message.tsx
@@ -6,6 +6,6 @@ import { ComponentProps } from 'react';
 type Props = ComponentProps<typeof PostMessageComponent>;
 
 export const PostMessage = ({ questionId, userId }: Props) => {
-  // NOTE: 現場はcomponent内に色々ロジック書いてるのでこのContainerの存在価値ない
+  // NOTE: 現状はcomponent内に色々ロジック書いてるのでこのContainerの存在価値ない
   return <PostMessageComponent questionId={questionId} userId={userId} />;
 };
