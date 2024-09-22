b9dbcc761cf1fb76a3b6f3e296dda45df74406b0
Naoki Takahashi <naoki.tkb@gmail.com>
2024-09-22 12:47:22 +0900

    fix

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,16 +1,16 @@
-import { signInAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
-import { SubmitButton } from "@/components/submit-button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Link from "next/link";
+import { signInAction } from '@/app/actions';
+import { FormMessage, Message } from '@/components/form-message';
+import { SubmitButton } from '@/components/submit-button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import Link from 'next/link';
 
 export default function Login({ searchParams }: { searchParams: Message }) {
   return (
     <form className="flex-1 flex flex-col min-w-64">
       <h1 className="text-2xl font-medium">ログイン</h1>
       <p className="text-sm text-foreground">
-        アカウントをお持ちでないですか？{" "}
+        アカウントをお持ちでないですか？{' '}
         <Link className="text-foreground font-medium underline" href="/sign-up">
           サインアップ
         </Link>
@@ -33,9 +33,7 @@ export default function Login({ searchParams }: { searchParams: Message }) {
           placeholder="Your password"
           required
         />
-        <SubmitButton pendingText="ログイン" formAction={signInAction}>
-        ログイン
-        </SubmitButton>
+        <SubmitButton formAction={signInAction}>ログイン</SubmitButton>
         <FormMessage message={searchParams} />
       </div>
     </form>
